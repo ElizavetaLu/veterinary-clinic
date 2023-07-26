d67f6a0ca23b5cfa2a606cbaf1c5988ab4f61c83
Elizaveta <lurovaj@gmail.com>
2023-07-26 12:58:24 +0300

    contacts page

--- client/src/pages/contacts/Contacts.tsx
+++ client/src/pages/contacts/Contacts.tsx
@@ -1,8 +1,49 @@
+import clinics from "../../data/clinics";
 import "./Contacts.scss";
+import Map from "./components/map/Map";
 
 const Contacts = () => {
     return (
-        <div>Contacts</div>
+        <main className="contacts">
+            <div className="contacts__data">
+                <h2 className="contacts__title">Contacts</h2>
+                <ul className="contacts__list">
+                    <li className="contacts__item">
+                        <img className="contacts__item-icon" src="/images/icons/phone.png" alt="phone" />
+                        + 38(055) 675 7647
+                    </li>
+                    <li className="contacts__item">
+                        <img className="contacts__item-icon" src="/images/icons/email.png" alt="phone" />
+                        email@example.com
+                    </li>
+                    <li className="contacts__item contacts__item--withList">
+                        <div className="contacts__item">
+                            <img className="contacts__item-icon" src="/images/icons/location.png" alt="phone" />
+                            Our clinics:
+                        </div>
+
+                        <ul className="contacts__nested-list">
+                            {
+                                clinics.map(item => {
+                                    return (
+                                        <li
+                                            key={item._id}
+                                            className="contacts__nested-item"
+                                        >
+                                            {item.address}
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="contacts__map">
+                <Map />
+            </div>
+        </main>
     )
 };
 
---
+++ client/src/pages/contacts/components/map/Map.tsx
@@ -0,0 +1,47 @@
+import { useRef } from 'react';
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import "./Map.scss"
+
+const containerStyle = {
+    width: '100%',
+    height: '100%'
+};
+
+const center = {
+    lat: -3.745,
+    lng: -38.523
+};
+
+const Map = () => {
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "API"
+    }) 
+    
+    const mapRef = useRef(null)
+
+    const onLoad = (map: any) => {
+        mapRef.current = map
+    }
+    const onUnmount = (map: any) => {
+        mapRef.current = null
+    }
+
+
+    if (!isLoaded) return <div>Pipka</div>
+
+    return (
+        <div className="map-container">
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+            />
+        </div>
+    )
+};
+
+export default Map
